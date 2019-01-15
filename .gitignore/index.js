85c6b8b5db75a9139ba53d5f9afd74b6d19a436b
dhaorix <46676399+dhaorix@users.noreply.github.com>
2019-01-15 17:02:06 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -59,7 +59,7 @@ bot.on('guildMemberRemove', member =>{
     if (args[0].toLowerCase() === prefix + "clear") {
         if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
         let count = args[1]
-        if (!count) return message.channel.send("Veuillez indiquer un nombre de messages à supprimer")
+        if (!count) return message.channel.send("Veuillez indiquer un nombre de messages à supprimer entre 1 et 100")
         if (isNaN(count)) return message.channel.send("Veuillez indiquer un nombre valide")
         if (count < 1 || count > 100) return message.channel.send("Veuillez indiquer un nombre entre 1 et 100")
         message.channel.bulkDelete(parseInt(count) + 1)
