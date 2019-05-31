c146f38a35e0e0151d824fcacca17a64f4405b06
dhaorix <46676399+dhaorix@users.noreply.github.com>
2019-05-31 16:01:20 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -144,6 +144,7 @@ bot.on('message', message =>{
         .addField("**:frame_photo:-IMAGE-:frame_photo:**", "``$kiss``, ``$hug``, ``$neko`` NSFW: ``$hentai``, ``$nude``")
         .addField("**:notes:-MUSIC-:notes:**", "``ARRIVE BIENTOT``")
         .addField("**:ring:-MARIAGE-:ring:**", "``ARRIVE BIENTOT``")
+        .addField("**:page_with_curl:-TICKET-:page_with_curl: **", "``$new``, ``$close``")
         .setFooter("help | bot by dhaorix")
         message.channel.sendMessage(help_embed);  
     }
@@ -492,6 +493,102 @@ bot.on('message', message =>{
         
         message.channel.send(embedAvatar);
     }  
+  
+      if (message.content.toLowerCase().startsWith(prefix + `new`)) {
+
+        const reason = message.content.split(" ").slice(1).join(" ");
+    
+        //if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+    
+        if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
+    
+        message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+    
+            let role = message.guild.roles.find("name", "Support Team");
+    
+            let role2 = message.guild.roles.find("name", "@everyone");
+    
+            c.overwritePermissions(role, {
+    
+                SEND_MESSAGES: true,
+    
+                READ_MESSAGES: true
+    
+            });
+    
+            c.overwritePermissions(role2, {
+    
+                SEND_MESSAGES: false,
+    
+                READ_MESSAGES: false
+    
+            });
+    
+            c.overwritePermissions(message.author, {
+    
+                SEND_MESSAGES: true,
+    
+                READ_MESSAGES: true
+    
+            });
+    
+            message.channel.send(`:white_check_mark: Le ticket a bien été crée , #${c.name}.`);
+    
+            const embed = new Discord.RichEmbed()
+    
+            .setColor(0xCF40FA)
+    
+            .addField(`Hey ${message.author.username}!`, `Un Administrateur ou un modo viendra bientôt a vous ! Patienter quelque instant...`)
+    
+            .setTimestamp();
+    
+            c.send({ embed: embed });
+    
+        }).catch(console.error);
+    
+    }
+
+
+    
+    if (message.content.toLowerCase().startsWith(prefix + `close`)) {
+    
+        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+    
+    
+    
+        message.channel.send(`Etes vous sûr? Confirmer pour fermer le ticket : \nTo confirm, type \`-confirm\`. Vous avez 10 seconde, après le délai, le message sera supprimé.`)
+    
+        .then((m) => {
+    
+          message.channel.awaitMessages(response => response.content === '-confirm', {
+    
+            max: 1,
+    
+            time: 10000,
+    
+            errors: ['time'],
+    
+          })
+    
+          .then((collected) => {
+    
+              message.channel.delete();
+    
+            })
+    
+            .catch(() => {
+    
+              m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+    
+                  m2.delete();
+    
+              }, 3000);
+    
+            });
+    
+        });
+    
+    }
   
 });
 
