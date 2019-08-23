12b0b82b02313031365683dff588894ff17a20ce
dhaorix <46676399+dhaorix@users.noreply.github.com>
2019-08-23 17:03:58 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -521,12 +521,50 @@ bot.on('message', message =>{
         message.channel.sendEmbed(fun_embed);
     }
 
-  var randlovecalc = 0
-function random(min,max){
-    min=Math.ceil(0);
-    max=Math.floor(5);
-    randnum=Math.floor(Math.random()*(max-min+1)+min);
-}
+  	     var randlovecalc = 0	
+	    function random(min, max){
+        min = Math.ceil(0);
+        max = Math.floor(100);
+        randlovecalc = Math.floor(Math.random() * (max - min +1)+ min);
+    }
+
+    if(message.content === "$lovecalc"){
+        message.reply("Merci d'utiliser la commande correctement avec ``$lovecalc <utilisateur>``")
+      }else{
+        if(message.content.startsWith("$lovecalc")){
+          random()
+          let lovecal = String(randlovecalc)
+          let memberd = message.mentions.members.first()
+          var colorlove = "RANDOM"
+          var textlove = "abc"
+          var imglove = ".jpg"
+          if(randlovecalc >= 85){
+            colorlove = "#00FF00"
+            textlove = "A croire qu'ils sont fait l'un pour l'autre !"
+            imglove = "https://cdn.discordapp.com/attachments/506983307897077770/546687125861433344/tumblr_n5rfnvvj7H1t62gxao1_500.gif"
+          }
+          if((randlovecalc < 85) && (randlovecalc >= 50)){
+            colorlove = "#FFA500"
+            textlove ="Qui ne tente rien n'a rien."
+            imglove = "https://cdn.discordapp.com/attachments/506983307897077770/546691835360182282/p.gif"
+          }
+          
+          if((randlovecalc < 50) && (randlovecalc >= 25)){
+            colorlove = "#FFA500"
+            textlove ="Qui ne tente rien n'a rien."
+            imglove = "https://cdn.discordapp.com/attachments/506983307897077770/546687046450937869/tenor.gif"
+          }
+          if((randlovecalc < 25) && (randlovecalc >= 1)){
+            colorlove = "#FF0000"
+            textlove = "Rien à faire.. Clairement."
+            imglove = "https://cdn.discordapp.com/attachments/506983307897077770/546687127207804929/giphy.gif"
+          }
+          var embedlove = new Discord.RichEmbed()
+          .setColor(`${colorlove}`)
+          .setDescription(`Le love score de ${message.author} avec ${memberd} est de ${lovecal} :heartbeat: !!. \n ${textlove}`)
+          .setImage(`${imglove}`)
+          message.channel.sendEmbed(embedlove)
+        }};
 
     if(message.content.startsWith(prefix + 'pileouface')) {
         res=getRandomInt(2)
