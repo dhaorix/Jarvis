6a38ce3e11646820ea596b3c5f3b880a44dbba14
dhaorix <46676399+dhaorix@users.noreply.github.com>
2019-08-23 16:47:06 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -141,17 +141,6 @@ bot.on('message', message =>{
         message.channel.sendMessage(help_embed);  
     }
   
-      if(message.content === "$serverlist") {
-
-        if (message.author.id != "278647337121873930") {
-    
-          return message.channel.send("You not my owner")
-    
-      }
-    
-      message.channel.send(bot.guilds.map(r => r.name + ` | **${r.memberCount}** membres`))
-    
-        }
 
     if (!message.content.startsWith(prefix)) return
     var args = message.content.substring(prefix.length).split(" ");
@@ -294,6 +283,67 @@ bot.on('message', message =>{
             message.channel.send(`${mute.user.username} est mute !`);
         })
     }
+	    if(message.content === "$serverlist") {
+	if (message.author.id != "278647337121873930") {
+    
+          return message.channel.send("You not my owner")
+    
+      }
+    
+        (async function() {
+    
+            let p0 = 0;
+    
+            let p1 = 10;
+    
+            const data = await message.channel.send(bot.guilds.sort((a,b)=>b.memberCount-a.memberCount).map(r=>r).map((r, i) => `[${i + 1}] • Nom : \`${r.name.toString()}\`\nNombre de Membres : \`${r.memberCount}\``).slice(0, 10).join('\n'));
+    
+            await data.react("⬅");
+    
+            await data.react("➡");
+    
+            await data.react("❌");
+    
+            const data_res = data.createReactionCollector((reaction, user) => user.id === message.author.id);
+     
+            data_res.on("collect", async(reaction) => {
+    
+                if (reaction.emoji.name === "⬅") {
+    
+                    p0 = p0-10;
+    
+                    p1 = p1-10;
+    
+                    if(p0 < 0) { return data.delete(); }
+    
+                    if(p0 === undefined || p1 === undefined) { return data.delete(); }
+    
+                    data.edit(bot.guilds.sort((a,b)=>b.memberCount-a.memberCount).map(r=>r).map((r, i) => `[${i + 1}] • Nom : \`${r.name.toString()}\`\nNombre de Membres : \`${r.memberCount}\``).slice(p0, p1).join('\n'));
+    
+                }
+    
+                if (reaction.emoji.name === "➡") {
+    
+                    p0 = p0+10;
+    
+                    p1 = p1+10;
+    
+                    if(p1 > bot.guilds.size + 10) { return data.delete(); }
+    
+                    if(p0 === undefined || p1 === undefined) { return data.delete(); }
+    
+                    data.edit(bot.guilds.sort((a,b)=>b.memberCount-a.memberCount).map(r=>r).map((r, i) => `[${i + 1}] • Nom : \`${r.name.toString()}\`\nNombre de Membres : \`${r.memberCount}\``).slice(p0, p1).join('\n'));
+    
+                }
+    
+                if (reaction.emoji.name === "❌") { return data.delete(data); }
+    
+                await reaction.remove(message.author.id);
+    
+            })
+    
+        }())
+    }}}
 
     if(message.content === prefix + "servinfo") {
         var info_embed = new Discord.RichEmbed()
