69b0e1a4e0a450db54df1f6d0c57ab7eb4267a6d
dhaorix <46676399+dhaorix@users.noreply.github.com>
2019-08-23 17:34:53 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -93,6 +93,38 @@ bot.on('message', message => {
     }
 });
 
+bot.on('guildMemberAdd', member =>{
+
+    let embed = new Discord.RichEmbed()
+
+        .setColor("#40A497")
+
+        .setDescription(':tada: **' + member.user.username + '** a rejoint ' + member.guild.name)
+
+        .setFooter('Nous sommes désormais ' + member.guild.memberCount)
+
+    member.guild.channels.get('410498158246887434').send(embed)
+
+    member.addRole('410494803680428042')
+
+});
+
+
+
+bot.on('guildMemberRemove', member =>{
+
+    let embed = new Discord.RichEmbed()
+
+         .setColor("#40A497")
+
+        .setDescription(':cry: **' + member.user.username + '** a quitté ' + member.guild.name)
+
+        .setFooter('Nous sommes désormais ' + member.guild.memberCount)
+
+    member.guild.channels.get('410498158246887434').send(embed).catch(console.error);
+
+});
+
 bot.on("message", message => {
     if (!message.guild) return
     let args = message.content.trim().split(/ +/g)
