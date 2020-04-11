8c9e428cdf746b976269266e69d3cd1757304f96
dhaorix <46676399+dhaorix@users.noreply.github.com>
2020-04-11 17:20:09 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,7 +11,17 @@ client.on( 'ready', function () {
 
 client.login(process.env.TOKEN)
 
+client.on('message', function(message) {
+    if (message.content == "/clear") {
+        if (message.member.hasPermission("MANAGE_MESSAGES")) {
+            message.channel.fetchMessages()
+               .then(function(list){
+                    message.channel.bulkDelete(list);
+                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})                        
+        }
+    }
 
+});
 
 client.on("message", async message => {
 
